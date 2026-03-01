61ee6325861356ea77cf3f322db8e271aedbd3c7
Misael <misaelborges1981@gmail.com>
2026-02-28 23:19:41 -0300

    Refatoração do header

--- src/components/Header/components/HeaderSheet/index.tsx
+++ src/components/Header/components/HeaderSheet/index.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import GradientButton from "@/components/GradientButton";
 import { Button } from "@/components/ui/button";
 import {
   Sheet,
   SheetContent,
   SheetDescription,
+  SheetFooter,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
@@ -36,33 +38,26 @@ const HeaderSheet = () => {
             Gerencie seu estabelecimento de forma eficiente.
           </SheetDescription>
         </SheetHeader>
-        <nav className="w-full p-4">
-          <ul className="w-full space-y-4 font-bold">
-            <li className="border-b border-orange-300 py-2 text-lg">
-              <Link
-                href="https://rangooo.vercel.app/"
-                onClick={() => setOpen(false)}
-                className="text-orange-600"
-              >
-                App Rangooo
-              </Link>
-            </li>
-
+        <nav className="mt-6 flex flex-col justify-between h-full gap-4">
+          <div className="flex flex-col gap-4 p-6">
             {linksPage.map((item) => (
-              <li
+              <Link
                 key={item.id}
-                className="border-b border-orange-300 py-2 text-lg"
+                href={item.href}
+                onClick={() => setOpen(false)}
+                className="pb-2 border-b border-orange-600 text-lg font-semibold text-orange-600 transition-all duration-300 hover:bg-orange-50 hover:text-orange-700"
               >
-                <Link
-                  href={item.href}
-                  onClick={() => setOpen(false)}
-                  className="text-orange-600 block w-full"
-                >
-                  {item.label}
-                </Link>
-              </li>
+                {item.label}
+              </Link>
             ))}
-          </ul>
+          </div>
+          <SheetFooter className="mt-4 border-t space-y-4 border-orange-200 p-6">
+            <GradientButton href="https://rangooo.vercel.app/">
+              App Rangooo
+            </GradientButton>
+
+            <GradientButton href="/cadastro">Teste Grátis</GradientButton>
+          </SheetFooter>
         </nav>
       </SheetContent>
     </Sheet>
--- src/components/Header/components/NavMenu/index.tsx
+++ src/components/Header/components/NavMenu/index.tsx
@@ -1,27 +1,14 @@
-import { Button } from "@/components/ui/button";
+import GradientButton from "@/components/GradientButton";
 import { linksPage } from "@/helpers/links-page-menu";
-import Link from "next/link";
 
 const NavMenu = () => {
   return (
     <nav className="w-full">
       <ul className="flex w-full items-center gap-6 font-bold">
         <li className="group relative">
-          <Button
-            variant="ghost"
-            className="group relative w-full transform overflow-hidden rounded-xl bg-linear-to-r from-orange-500 to-red-500 py-4 text-lg font-bold text-white shadow-lg transition-all duration-300 hover:scale-105 hover:from-orange-600 hover:to-red-600 hover:shadow-2xl active:scale-95"
-            asChild
-          >
-            <Link href="https://rangooo.vercel.app/">
-              {/* Efeito de brilho no hover */}
-              <span className="absolute inset-0 translate-x-full bg-linear-to-r from-white/50 to-transparent transition-transform duration-700 group-hover:translate-x-full" />
-
-              {/* Texto do botão */}
-              <span className="relative z-10 text-white hover:text-yellow-100">
-                App Rangooo
-              </span>
-            </Link>
-          </Button>
+          <GradientButton href="https://rangooo.vercel.app/">
+            App Rangooo
+          </GradientButton>
         </li>
         {linksPage.map((item) => (
           <li key={item.id} className="group relative">
@@ -35,21 +22,7 @@ const NavMenu = () => {
           </li>
         ))}
         <li>
-          <Button
-            variant="ghost"
-            className="group relative w-full transform overflow-hidden rounded-xl bg-linear-to-r from-orange-500 to-red-500 py-4 text-lg font-bold text-white shadow-lg transition-all duration-300 hover:scale-105 hover:from-orange-600 hover:to-red-600 hover:shadow-2xl active:scale-95"
-            asChild
-          >
-            <Link href="/cadastro">
-              {/* Efeito de brilho no hover */}
-              <span className="absolute inset-0 translate-x-full bg-linear-to-r from-white/50 to-transparent transition-transform duration-700 group-hover:translate-x-full" />
-
-              {/* Texto do botão */}
-              <span className="relative z-10 text-white hover:text-yellow-100">
-                Teste Grátis
-              </span>
-            </Link>
-          </Button>
+          <GradientButton href="/cadastro">Teste Grátis</GradientButton>
         </li>
       </ul>
     </nav>
